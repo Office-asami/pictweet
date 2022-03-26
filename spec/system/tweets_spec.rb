4d8f03b5c515d8ecb2e04187889c12b84c77c659
Office-asami <s-wakita@office-asami.com>
2022-03-26 17:50:43 +0900

    first commit

---
+++ spec/system/tweets_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe 'ツイート投稿', type: :system do
+  before do
+    @user = FactoryBot.create(:user)
+    @tweet_text = Faker::Lorem.sentence
+    @tweet_image = Faker::Lorem.sentence
+  end
+
+  pending "add some scenarios (or delete) #{__FILE__}"
+end
